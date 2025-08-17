464da85492761e8760b6f76a75962a226491016b
Tshegofatso Mkhabela <tshego300@gmail.com>
2025-08-17 02:33:58 +0200

    feat: add AI-powered assistant for MzansiMarket users

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChatOverlay } from "./ChatBotOverlay/ChatOverlay";
+
+const App = () => {
+  return (
+    <>
+      <ChatOverlay />
+    </>
+  );
+};
+
+export default App;
